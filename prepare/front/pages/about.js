de2307cf7e5dcfee5811dc18b0763aedf7eafa03
purrsong-kevin <kevin@purrsong.com>
2020-12-18 18:44:14 +0900

    Fix/typo

--- prepare/front/pages/about.js
+++ prepare/front/pages/about.js
@@ -48,7 +48,12 @@ const About = () => {
   );
 };
 
-export const getStaticProps = wrapper.getStaticProps(async context => {
+export const getServerSideProps = wrapper.getServerSideProps(async context => {
+  const cookie = context.req ? context.req.headers.cookie : '';
+  axios.defaults.headers.Cookie = '';
+  if (context.req && cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
   context.store.dispatch({
     type: LOAD_USER_REQUEST,
     data: 1
