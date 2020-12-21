49fd1f786e3462a27e07acb4c1919cd25a6c6e16
purrsong-kevin <kevin@purrsong.com>
2020-12-21 15:17:35 +0900

    add db password for production

--- prepare/back/config/config.js
+++ prepare/back/config/config.js
@@ -11,14 +11,14 @@ module.exports = {
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": process.env.DB_PASSWORD,
     "database": "react-nodebird",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": null,
+    "password": process.env.DB_PASSWORD,
     "database": "react-nodebird",
     "host": "127.0.0.1",
     "dialect": "mysql"
