0c62170243d4a70ef3f920336c7c1e4b75547990
purrsong-kevin <kevin@purrsong.com>
2020-12-21 14:25:42 +0900

    change port

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -70,6 +70,6 @@ app.use('/posts', postsRouter);
 app.use('/user', userRouter);
 app.use('/hashtag', hashtagRouter);
 
-app.listen(3065, () => {
+app.listen(80, () => {
   console.log('Server listen 3065!');
 });
